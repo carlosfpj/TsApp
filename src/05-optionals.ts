2595d6eb4566e4c56697200fa2470ba5114a5584
carlosfpj <cafer89@gmail.com>
2023-01-07 22:21:25 -0500

    optionals

---
+++ src/05-optionals.ts
@@ -0,0 +1,28 @@
+export const createProduct = (
+  // why not the default in here?
+  id: string | number,
+  isNew: boolean,
+  stock?:  number,
+  param: string = 'default',
+) => {
+  return {
+    id,
+    // stock: stock || true,
+    //but here?
+    stock: stock ?? true,
+    // isNew: isNew || 10
+    isNEw: isNew ?? 10,
+    param
+  }
+}
+
+const p1 = createProduct(1, true, 12)
+console.log(p1);
+
+const p2 = createProduct(1, true);
+console.log(p2);
+
+const p3 = createProduct(1, false, 0);
+console.log(p3);
+
+
